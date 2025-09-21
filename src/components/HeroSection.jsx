8060b25959b364ae89ddfd41c9d273f5b9a95fbc
jagnyadatta <jagnyadattadalai99215@gmail.com>
2025-09-21 11:29:58 +0530

    fixed button

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useTheme } from "../context/ThemeContext";
-import { Button } from "./ui/button";
+import { Button } from "./ui/Button.jsx";
 import { X } from "lucide-react";
 import { Link } from "react-router-dom";
 
